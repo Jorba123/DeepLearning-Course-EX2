af392cb1e1f6799367b24b59b055e8c9850e0b89
Felix Schober <felix.schober@tum.de>
2017-11-26 11:07:07 +0100

    add affine layer forward pass

--- dl4cv/layers.py
+++ dl4cv/layers.py
@@ -24,7 +24,8 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You     #
     # will need to reshape the input into rows.                                 #
     #############################################################################
-    pass
+    x = np.reshape(x, (x.shape[0], -1))
+    out = np.dot(x, w) + b
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
@@ -340,3 +341,44 @@ def softmax_loss(x, y):
     dx[np.arange(N), y] -= 1
     dx /= N
     return loss, dx
+
+
+
+
+
+
+
+
+
+
+
+
+# Testing
+# from dl4cv.classifiers.fc_net import *
+# from dl4cv.data_utils import get_CIFAR10_data
+# from dl4cv.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+# from dl4cv.solver import Solver
+# # data = get_CIFAR10_data()
+# # for k, v in data.items():
+# #     print('%s: ' % k, v.shape)
+#
+# # Test the affine_forward function
+#
+# num_inputs = 2
+# input_shape = (4, 5, 6)
+# output_dim = 3
+#
+# input_size = num_inputs * np.prod(input_shape)
+# weight_size = output_dim * np.prod(input_shape)
+#
+# x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs, *input_shape)
+# w = np.linspace(-0.2, 0.3, num=weight_size).reshape(np.prod(input_shape), output_dim)
+# b = np.linspace(-0.3, 0.1, num=output_dim)
+#
+# out, _ = affine_forward(x, w, b)
+# correct_out = np.array([[ 1.49834967,  1.70660132,  1.91485297],
+#                         [ 3.25553199,  3.5141327,   3.77273342]])
+#
+# # Compare your output with ours. The error should be around 1e-9.
+# print('Testing affine_forward function:')
+# #print('difference: ', rel_error(out, correct_out))

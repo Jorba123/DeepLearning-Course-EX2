dbdbc5a37a82827588ac5724fc9bf75f17f31e6e
Felix Schober <felix.schober@tum.de>
2017-11-26 23:21:16 +0100

    Add SGD + Momentum

--- dl4cv/optim.py
+++ dl4cv/optim.py
@@ -65,7 +65,11 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in   #
     # the next_w variable. You should also use and update the velocity v.       #
     #############################################################################
-    pass
+    gamma = config.get('momentum')
+    alpha = config.get('learning_rate')
+    v = gamma * v - alpha * dw
+    next_w = w + v
+
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
